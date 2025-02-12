174d09b7f6001446b803ffee0bb91e32fe587fcd
sdl74 <sdl74@case.edu>
2025-02-12 14:39:16 -0500

    added hash function & password.txt initialization

--- PasswordManager.java
+++ PasswordManager.java
@@ -3,8 +3,22 @@ import java.io.FileWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
 
 public class PasswordManager {
+
+    // string used to verify user entered the correct password
+    private static String verificationString = "verify";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // User inputs the initial passcode
@@ -69,5 +83,62 @@ public class PasswordManager {
                 }
             }
         }
+    }
+
+    // takes a string pass and salt salt and returns the hashed string using PBKDF2
+    // this function is used to generate the key for encryption and decryption
+    private static SecretKey hash(String pass, String salt){
+        // generate the key
+        try{
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(pass.toCharArray(), salt.getBytes(), 10000, 256);
+            SecretKey secret = new SecretKeySpec(skf.generateSecret(spec).getEncoded(), "AES");
+
+            // return the key encoded in base64
+            return secret;
+        }catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // this function generates the password.txt file and populates it with the salt string (encoded in base64) and a verification token
+    // input is the super secret password which will be used to generate the key used for encryption and decryption
+    private static void createPasswordFile(String password){
+        // generate random salt string
+        String salt = getNewSalt();
+
+        // hash the password to make the key
+        SecretKey key = hash(password, salt);
+
+        // encrypt "verify" to create verification token
+        String verificationToken = encrypt(verificationString, key);
+
+        // write the salt & verificationToken to password.txt
+        // (just print it out for now)
+        System.out.println("salt: " + salt);
+        System.out.println("verification token: " + verificationToken);
+    }
+
+    // generates a random salt string
+    private static String getNewSalt(){
+        return "salt";
+    }
+
+    // encrypts a password using the given key
+    // returns the encrypted password in base64
+    private static String encrypt(String password, SecretKey key){
+        return "";
+    }
+
+    // decrypts the given token with the key and returns the decrypted password
+    private static String decrypt(String token, SecretKey key){
+        return "";
+    }
+
+    // generates initialilzation vector for AES algorithm
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
     }
 }
